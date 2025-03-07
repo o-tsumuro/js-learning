5f816f5a98d943b4b0fb4e554b9dba9562bbbd81
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-07 23:06:51 +0900

    filter関数

--- tutorial/list2.js
+++ tutorial/list2.js
@@ -1,4 +1,9 @@
 /* split関数 -> 引数に渡した文字で区切り配列に分割する*/
 const data = "札幌,仙台,東京,名古屋,大阪,福岡";
-const cities = data.split(",");
-console.log(cities);
+const cities1 = data.split(",");
+console.log(cities1); //[ '札幌', '仙台', '東京', '名古屋', '大阪', '福岡' ]
+
+/* join関数 -> レシーバーの配列を引数で区切った形の文字列にする */
+const cities2 = ['札幌', '仙台', '東京', '名古屋', '大阪', '福岡'];
+const commaSeparated = cities2.join("");
+console.log(commaSeparated); //札幌仙台東京名古屋大阪福岡
