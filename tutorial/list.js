36c1a7aa91e48148d45accbc3b980a12f25232ff
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-07 22:31:22 +0900

    unshift関数

--- tutorial/list.js
+++ tutorial/list.js
@@ -3,3 +3,8 @@ const cities1 = ["東京", "大阪"];
 const newLength = cities1.push("神戸");
 console.log(cities1);
 console.log(newLength);
+
+/* 要素を先頭に追加する -> unshift関数 */
+const cities2 = ["東京", "大阪"];
+cities2.unshift("札幌");
+console.log(cities2)
