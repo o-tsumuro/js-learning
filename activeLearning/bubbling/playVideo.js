efcc82fb2637570a9db77f7b89a2ca62aff4d075
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-10 10:34:41 +0900

    修正前例コード追加

---
+++ activeLearning/bubbling/playVideo.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector("button");
+const box = document.querySelector("div");
+const video = document.querySelector("video");
+
+// 要件
+// btnをクリックでvideoを表示, videoをクリックで再生開始,boxをクリックで動画を非表示
+
+// 以下のコードでは動画の再生(videoクリック時)と同時に非表示になってしまう
+// -> box(div)はvideoの親要素であるためバブリングによってboxのクリックも呼び出される
+// btn.addEventListener("click", () => box.classList.remove("hidden"));
+// video.addEventListener("click", () => video.play());
+// box.addEventListener("click", () => box.classList.add("hidden"));
