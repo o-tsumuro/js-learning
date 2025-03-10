4b1155ab771b6c4d81caea02979427e56f79d86f
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-10 10:37:03 +0900

    修正後コード追加

--- activeLearning/bubbling/playVideo.js
+++ activeLearning/bubbling/playVideo.js
@@ -10,3 +10,13 @@ const video = document.querySelector("video");
 // btn.addEventListener("click", () => box.classList.remove("hidden"));
 // video.addEventListener("click", () => video.play());
 // box.addEventListener("click", () => box.classList.add("hidden"));
+
+// stopPropagation()でこの問題を修正(バブリングするのを防ぐ)
+btn.addEventListener("click", () => box.classList.remove("hidden"));
+
+video.addEventListener("click", (event) => {
+  event.stopPropagation();
+  video.play();
+});
+
+box.addEventListener("click", () => box.classList.add("hidden"));
