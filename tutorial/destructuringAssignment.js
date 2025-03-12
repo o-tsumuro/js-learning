76cd3498ad812f9d7bb9491fc133e1d841bbaff1
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-12 16:48:18 +0900

    配列の分割代入メモ

--- tutorial/destructuringAssignment.js
+++ tutorial/destructuringAssignment.js
@@ -1,4 +1,4 @@
-// オブジェクトの分割代入
+// --- オブジェクトの分割代入 ---
 // 変数名はオブジェクトのキーと一致する必要がある
 // 順番は関係ない
 // 「:」で別名を付けることができる
@@ -10,3 +10,16 @@ console.log(age); // 25
 console.log(city); // Tokyo
 console.log(userName); // Alice
 console.log(country); // Japan
+
+// ---配列の分割代入---
+// 変数名は自由に決められる
+// 順番が重要
+// 「,」を使って不要な要素を飛ばすことができる
+// 「...」を使うと残りの要素をまとめて取得できる
+const colors = ["red", "green", "blue", "black", "white", "gray", "skyblue"];
+const [firstColor, secondColor, , forthColor, ...rest] = colors;
+
+console.log(firstColor); // red
+console.log(secondColor); // green
+console.log(forthColor); // black
+console.log(rest); // [ 'white', 'gray', 'skyblue' ]
